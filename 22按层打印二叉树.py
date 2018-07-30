1166aff5f90c25408f21d3e0248befca7002b15e
Leo4ML <838551198@qq.com>
2018-07-30 21:50:15 +0800

    增加第22题

--- 22按层打印二叉树.py
+++ 22按层打印二叉树.py
@@ -1,18 +1,20 @@
+# -*- coding: utf-8 -*-
+
 class Solution:
     # 返回从上到下每个节点值列表，例：[1,2,3]
     def PrintFromTopToBottom(self, root):
         L=[]
+        s=[]
         # write code here
         if root==None:
-            return None
+            return s
         else:
             L.append(root)
         while L!=[]:
             head=L.pop(0)
-            print (head.val)
+            s.append(head.val)
             if head.left!=None:
                 L.append(head.left)
             if head.right!=None:
                 L.append(head.right)
-        
-
+        return s

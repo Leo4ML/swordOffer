d6706e62e8d2a6e1d3b3084216a97cec623184ed
Leo4ML <838551198@qq.com>
2018-08-02 11:46:53 +0800

    31增加非递归版本

--- 31.整数中1出现的次数（1到n）.py
+++ 31.整数中1出现的次数（1到n）.py
@@ -15,3 +15,18 @@ class Solution:
                 k=int(str(n)[0])
 #最高位上1出现的次数+去掉高位后剩下位数*轮次（如，231，就是0-99,100-199，两轮）+后面部分出现1的次数（231就是，200-231）
                 return 10**(l-1)+k*self.NumberOf1Between1AndN_Solution(10**(l-1)-1)+self.NumberOf1Between1AndN_Solution(n-k*10**(l-1))
+            
+'''
+补充非递归版本，通i依次为1,10,100,1000....，来统计各位上出现1的次数
+class Solution:
+    def NumberOf1Between1AndN_Solution(self, n):
+        # write code here
+        res=0
+        i=1
+        while i<=n:
+            a=n/i
+            b=n%i
+            res+=(a+8)/10*i+(a%10==1)*(b+1)
+            i*=10
+        return res
+'''

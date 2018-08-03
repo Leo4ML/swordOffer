3516637db0aae523f959e1a54ff6db6a46b02fab
Leo4ML <838551198@qq.com>
2018-08-03 21:05:51 +0800

    增加一种更巧妙的方法

--- 30.连续子数组的最大和.py
+++ 30.连续子数组的最大和.py
@@ -11,3 +11,15 @@ class Solution:
             ans.append(max(res))
             k+=1
         return max(ans)
+    
+'''
+还可以这么简单，且不用额外空间！
+# -*- coding:utf-8 -*-
+class Solution:
+    def FindGreatestSumOfSubArray(self, array):
+        maxsum,cursum=array[0],array[0]
+        for i in array[1:]:
+            cursum=max(cursum+i,i)
+            maxsum=max(maxsum,cursum)
+        return maxsum
+'''
